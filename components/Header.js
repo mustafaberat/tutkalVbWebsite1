b3c769c7eeb43e2f1a4d7baa42b1d7ee31d0ca00
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2020-06-16 00:21:34 +0300

    head and header

---
+++ components/Header.js
@@ -0,0 +1,35 @@
+import React from "react"
+import Link from 'next/link'
+
+
+export default function Header() {
+    return (
+        <div className="navbar">
+            {/* <img src="/glue.svg" /> */}
+            <h3>LOGO</h3>
+            <nav className="navbar navbar-expand-lg navbar-dark">
+
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">Anasayfa <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/kurumsal">Kurumsal <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item ">
+                            <a className="nav-link" href="/arge">AR-GE <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/iletisim">İletişim</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div >
+    )
+}
---
+++ components/MyHead.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Head from 'next/head'
+const MyHead = () => {
+    return (
+        <Head>
+            <title>TITLE</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="icon" href="/glue.svg" />
+            <meta name="theme-color" content="#fff" />
+            <meta name="description" content="DescriptionnnnnnnDescriptionnnnnnn"></meta>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossOrigin="anonymous" />
+        </Head>
+    );
+}
+
+export default MyHead
