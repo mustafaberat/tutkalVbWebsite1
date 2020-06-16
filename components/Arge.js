34b2f0dfc6c088a161186d111e6fb994e81563f4
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2020-06-16 13:38:02 +0300

    Responsive image adding in arge

--- components/Arge.js
+++ components/Arge.js
@@ -5,7 +5,10 @@ function Arge() {
     return (
         <section className="argeContainer">
             <h4>AR-GE</h4>
-            <img src="https://www.somafix.com.tr/cropped/?src=dosyalar/page_42/1562229262_1.jpg&w=600&h=300" />
+            <figure className="figure">
+                <img src="https://www.somafix.com.tr/cropped/?src=dosyalar/page_42/1562229262_1.jpg&w=600&h=300" className="figure-img img-fluid rounded" alt="Resim" />
+                <figcaption className="figure-caption">A caption for the above image.</figcaption>
+            </figure>
             <h6>Yapıya Bilimsel Yaklaşım,</h6>
             <p>Dünya inşaat sektörünün ihtiyaçlarına kalıcı çözümler sunma misyonu ile çalışan Soma Kimya olarak, AR-GE çalışmalarının üretici bir firma için öneminin farkındayız. Misyon ve vizyonumuzu hakkı ile gerçekleştirmek için kendi laboratuvar çalışmalarımızın yanı sıra dünyadaki gelişmeleri de yakından takip ederek yatırımlarımıza bu yönde devam ediyoruz.</p>
             <p>Ar-Ge Departmanı sektöre sunulan ürünlerin standardizasyonunun sağlanması ve kalitenin arttırılmasına yönelik çözümler üreterek araştırma ve geliştirme faaliyetlerine devam ediyor.
